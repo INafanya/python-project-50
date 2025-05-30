497af46deff2ef6ada34e6ef0c05671d6e699c9f
Andrey Solovyov <solovyov.net@gmail.com>
2025-05-31 01:33:37 +0300

    add parsing json files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 
+path_to_files = './gendiff/files'
 def main():
     parser = argparse.ArgumentParser(
                     prog='gendiff',
@@ -8,6 +10,26 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
+    
+    file1 = args.first_file
+    file2 = args.second_file
+    
+    # file1 = json.load(open(f'{path_to_files}/{first_file}'))
+    # file2 = json.load(open(f'{path_to_files}/{file2}'))
+    
+    with open(f'{path_to_files}/{file1}', 'r') as file:
+        file1_data = json.load(file)
+        
+    with open(f'{path_to_files}/{file2}', 'r') as file:
+        file2_data = json.load(file)
+
+    print('\nfile1 data:')
+    for data in file1_data:
+        print(f'{data} = {file1_data[data]}')
+    
+    print('\nfile2 data:')
+    for data in file2_data:
+        print(f'{data} = {file2_data[data]}')
 
 
 if __name__ == '__main__':
