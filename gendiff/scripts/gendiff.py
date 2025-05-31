f170b2103713934ea77574fa939006d024a27d63
Andrey Solovyov <solovyov.net@gmail.com>
2025-06-01 02:20:41 +0300

    fix gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -41,13 +41,13 @@ def generate_diff(file_path1, file_path2):
                             diff_result += f'    {key_1}: {file1_data[key_1]}\n'
                         else:
                             diff_result += f'  - {key_1}: {file1_data[key_1]}\
-                                \n  + {key_2}: {file2_data[key_2]}\n'
+\n  + {key_2}: {file2_data[key_2]}\n'
                 else:
                     diff_result += f'  + {key_2}: {file2_data[key_2]}\n'
         else:
             diff_result += f'  - {key_1}: {file1_data[key_1]}\n'
     diff_result += '}'
-    print(diff_result)
+    # print(diff_result)
     return diff_result
 
 
