af0d4d3aa8f46273b28ccd42eb4aa7e18e5a706d
Andrey Solovyov <solovyov.net@gmail.com>
2025-05-31 03:19:08 +0300

    add diff json

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,12 @@
 import argparse
 import json
 
-path_to_files = './gendiff/files'
+
 def main():
     parser = argparse.ArgumentParser(
                     prog='gendiff',
-                    description='Compares two configuration files and shows a difference.')
+                    description='Compares two configuration files and shows a difference.'
+                    )
     parser.add_argument('-f', '--format', type=str, help='set format of output')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
@@ -14,22 +15,46 @@ def main():
     file1 = args.first_file
     file2 = args.second_file
     
-    # file1 = json.load(open(f'{path_to_files}/{first_file}'))
-    # file2 = json.load(open(f'{path_to_files}/{file2}'))
-    
-    with open(f'{path_to_files}/{file1}', 'r') as file:
-        file1_data = json.load(file)
-        
-    with open(f'{path_to_files}/{file2}', 'r') as file:
-        file2_data = json.load(file)
+    generate_diff(file1, file2)
 
-    print('\nfile1 data:')
-    for data in file1_data:
-        print(f'{data} = {file1_data[data]}')
+
+def read_and_sort_json(json_file):
+    with open(f'{json_file}', 'r') as file:
+        data = json.load(file)
+    sorted_data = dict(sorted(data.items()))
+    return sorted_data
+
+
+def generate_diff(file_path1, file_path2):    
+    diff_result = '{\n'
     
-    print('\nfile2 data:')
-    for data in file2_data:
-        print(f'{data} = {file2_data[data]}')
+    file1_data = read_and_sort_json(file_path1)
+    file2_data = read_and_sort_json(file_path2)
+
+    # print('\nfile1 data:')
+    # for data in file1_data:
+    #     print(f'{data} = {file1_data[data]}')
+    
+    # print('\nfile2 data:')
+    # for data in file2_data:
+    #     print(f'{data} = {file2_data[data]}')
+    
+    for key_1 in file1_data:
+        if key_1 in file2_data:
+            for key_2 in file2_data:
+                if key_2 in file1_data:
+                    if key_1 == key_2:
+                        if file1_data[key_1] == file2_data[key_2]:
+                            diff_result += f'    {key_1}: {file1_data[key_1]}\n'
+                        else:
+                            diff_result += f'  - {key_1}: {file1_data[key_1]}\n  + {key_2}: {file2_data[key_2]}\n'
+                else:
+                    diff_result += f'  + {key_2}: {file2_data[key_2]}\n'
+        else:
+            diff_result += f'  - {key_1}: {file1_data[key_1]}\n'
+    diff_result += '}'
+    
+    print(diff_result)
 
 
 if __name__ == '__main__':
